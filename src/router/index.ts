b7b84402c910ed1bfd256e90ff32ab5b9ca60d3b
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-04-24 01:24:54 +0300

    Add health-check endpoint

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,14 @@ import usersRouter from "./users/router";
 /** @public */
 const router = Router();
 
+router.route("/")
+	.get((req, res) => {
+		res.json({
+			message: "💪",
+			healthy: true,
+		});
+	});
+
 router.use("/users", usersRouter);
 
 export default router;
