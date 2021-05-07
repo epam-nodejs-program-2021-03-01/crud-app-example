544a1830af9bf3e4336893ff4e3915358eb4eeb6
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-05-07 22:40:23 +0300

    Log database connection event

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import connect from "./db/connect";
 
 const { PORT, DATABASE_CONNECT_TIMEOUT } = process.env;
 
-connect({ timeout: DATABASE_CONNECT_TIMEOUT }).then(() => {
+connect({ timeout: DATABASE_CONNECT_TIMEOUT }).then(({ user, database, host, port }) => {
+	console.log(`Connected to database '${database}' at address '${host}:${port}' as user '${user}'`);
+
 	app.listen(PORT, () => {
 		console.log(`Server is listening on port ${PORT}`);
 	});
