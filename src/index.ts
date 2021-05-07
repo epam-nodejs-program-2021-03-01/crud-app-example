520ac67ed27d3de3d705c1ee6bf7d656ab910714
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-05-07 22:43:04 +0300

    Define synchronizeModels() function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import connect from "./db/connect";
+import synchronizeModels from "./db/synchronize-models";
 
 const { PORT, DATABASE_CONNECT_TIMEOUT } = process.env;
 
@@ -9,4 +10,6 @@ connect({ timeout: DATABASE_CONNECT_TIMEOUT }).then(({ user, database, host, por
 	app.listen(PORT, () => {
 		console.log(`Server is listening on port ${PORT}`);
 	});
+
+	synchronizeModels();
 });
