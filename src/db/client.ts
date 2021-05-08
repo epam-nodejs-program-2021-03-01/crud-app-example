5b33b70e524d14ecef1258db256612b008db36f2
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-05-08 20:01:47 +0300

    Properly log SQL queries

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,5 +1,6 @@
 import type { ClientConfig } from "pg";
 import { Sequelize } from "sequelize";
+import logger from "../log/logger";
 
 export * from "sequelize";
 
@@ -11,6 +12,9 @@ const client = new Sequelize(process.env.DATABASE_URL, {
 			require: true,
 			rejectUnauthorized: false, // allow self-signed certificates
 		},
+	},
+	logging(sql) {
+		logger.debug(sql);
 	},
 });
 
